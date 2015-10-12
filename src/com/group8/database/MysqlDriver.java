ef423cd41096ab45ed72e5fcb5e2e04cd43fc199
Linus Eiderström Swahn <es.linus@gmail.com>
2015-10-12 13:03:22 +0200

    Added mysqlDriver class that handles select and insert requests.

---
+++ src/com/group8/database/MysqlDriver.java
@@ -0,0 +1,159 @@
+package com.group8.database;
+
+import com.mysql.jdbc.MysqlParameterMetadata;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MysqlDriver {
+
+    protected ArrayList<Object> select(String query) {
+
+        ArrayList<Object> result = new ArrayList<>();
+
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://sql.smallwhitebird.com:3306/beerfinder";
+        String user = "Gr8";
+        String password = "group8";
+
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery(query);
+
+            ResultSetMetaData metaData = rs.getMetaData();
+
+            rs.next();
+
+            result = new ArrayList<>();
+
+            for(int i = 1; i<=metaData.getColumnCount(); i++)
+            {
+                result.add(rs.getObject(i));
+            }
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(MysqlDriver.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(MysqlDriver.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+
+        return result;
+    }
+
+    protected ArrayList<ArrayList> selectMany(String query) {
+
+        ArrayList<ArrayList> result = new ArrayList<>();
+
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://sql.smallwhitebird.com:3306/beerfinder";
+        String user = "Gr8";
+        String password = "group8";
+
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery(query);
+
+            ResultSetMetaData metaData = rs.getMetaData();
+
+            while(rs.next())
+            {
+                ArrayList<Object> row = new ArrayList<>();
+
+                for(int i = 1; i<=metaData.getColumnCount(); i++)
+                {
+                    row.add(rs.getObject(i));
+                }
+
+                result.add(row);
+            }
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(MysqlDriver.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(MysqlDriver.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+
+        return result;
+    }
+
+    public void insert(String query)
+    {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://sql.smallwhitebird.com:3306/beerfinder";
+        String user = "Gr8";
+        String password = "group8";
+
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery(query);
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(MysqlDriver.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(MysqlDriver.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+    }
+}
