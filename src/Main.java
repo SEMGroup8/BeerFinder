d2ab56296597466e0d3ec3c302914f80c5815b06
Linus Eiderström Swahn <es.linus@gmail.com>
2015-10-22 09:17:59 +0200

    Added Javafx implemetnation.

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,16 @@ import com.group8.database.*;
 
 import java.sql.*;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.stage.Stage;
+
 
 /**
  * Created by Shiratori on 12/10/15.
  */
-public class Main {
+public class Main extends Application{
 
 	String searchString;
     public static void main(String[] args)
@@ -28,5 +33,18 @@ public class Main {
                 System.out.println(row.get(j).toString());
             }
         }
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/group8/resources/views/homescreen.fxml"));
+
+        Scene scene = new Scene(root, 800, 600);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
     }
 }
