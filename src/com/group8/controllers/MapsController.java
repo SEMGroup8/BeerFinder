bc7a0c96d949a0195626fee688f7ce628866ba92
Andreas Fransson <Ankanx@gmail.com>
2015-11-05 22:44:39 +0100

    Made a textview showing my current location by specific GPS coordinates.

--- src/com/group8/controllers/MapsController.java
+++ src/com/group8/controllers/MapsController.java
@@ -1,5 +1,8 @@
 package com.group8.controllers;
 
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,10 +12,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
+import com.lynden.gmapsfx.javascript.object.Marker;
+import com.lynden.gmapsfx.javascript.object.MarkerOptions;
 
 
 /**
@@ -21,10 +31,16 @@ import java.util.ResourceBundle;
  * TODO learn the goglefx API and implement it
  *
  */
-public class MapsController implements Initializable {
+public class MapsController implements Initializable,MapComponentInitializedListener {
 
+    @FXML
     public Button Back;
+    @FXML
     public Button Home;
+    @FXML
+    public GoogleMapView mapView;
+    @FXML
+    public GoogleMap map;
 
     /**
      * Back button pressed takes you back to "result screen"
@@ -55,16 +71,50 @@ public class MapsController implements Initializable {
         main_stage.show();
     }
 
-
-    /**
-     *  Initialize maps controller
-     * @param location
-     * @param resources
-     */
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        // test output
-        System.out.println("Maps accsessed and initializeing!");
+    public void initialize(URL url, ResourceBundle rb) {
+        mapView.addMapInializedListener(this);
+    }
+
+
+    @Override
+    public void mapInitialized() {
+
+        LatLong andreasLocation = new LatLong(57.654382, 12.078235);
+
+
+
+        //Set the initial properties of the map.
+        MapOptions mapOptions = new MapOptions();
+
+        mapOptions.center(new LatLong(57.654382, 12.078235))
+                .mapType(MapTypeIdEnum.ROADMAP)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(true)
+                .zoom(15);
+
+        map = mapView.createMap(mapOptions);
+
+        //Add markers to the map
+        MarkerOptions markerOptions1 = new MarkerOptions();
+        markerOptions1.position(andreasLocation);
+
+
+        Marker andreasMarker = new Marker(markerOptions1);
+
+        map.addMarker( andreasMarker );
+
+
+        InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
+        infoWindowOptions.content("<h2>Andreas Grotta</h2>"
+                + "Här bor ett stort troll<br>"
+               + "tar 53 min till lindholmen med buss" );
+        InfoWindow andreasWindow = new InfoWindow(infoWindowOptions);
+       andreasWindow.open(map, andreasMarker);
     }
 
 }
