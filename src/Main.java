86cab57174af050ea2c7449aa8f4c7e5692209c0
Andreas Fransson <Ankanx@gmail.com>
2015-12-06 03:05:34 +0100

    Looped the clip, added custom clip made my neighbour , made the thread terminate when the program does.

--- src/Main.java
+++ src/Main.java
@@ -18,17 +18,16 @@ import java.io.IOException;
 /**
  * Created by Shiratori on 12/10/15.
  */
-public class Main extends Application{
+public class Main extends Application {
 
     public static String test;
+    File file = new File("src/8bit projekt.wav");
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         test = "Bajs";
 
-        for(int i = 0; i < test.length(); i++)
-        {
-            System.out.println(test.substring(i, i+1));
+        for (int i = 0; i < test.length(); i++) {
+            System.out.println(test.substring(i, i + 1));
         }
 
         launch(args);
@@ -40,40 +39,47 @@ public class Main extends Application{
         Navigation.primaryStage = stage;
         Parent root = FXMLLoader.load(getClass().getResource("/com/group8/resources/views/mainScene.fxml"));
         Scene scene = new Scene(root, 800, 600);
-       // scene.getStylesheets().add("com/group8/resources/css/resultTableCss.css");
+        // scene.getStylesheets().add("com/group8/resources/css/resultTableCss.css");
         stage.setTitle("BeerFinder Beta Test");
         stage.setScene(scene);
         stage.getIcons().add(new Image("file:src/com/group8/resources/Images/Icon.png"));
-        //play();
+        play();
         stage.show();
 
     }
 
     // DERP
     public void play() {
+        Thread derp = new Thread() {
+            @Override
+            public void run() {
 
-            new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        File file = new File("src/11k16bitpcm.wav");
-                        Clip clip = AudioSystem.getClip();
+
+                System.out.println("running -->");
+
+                try {
+                    Clip clip = AudioSystem.getClip();
                         clip.open(AudioSystem.getAudioInputStream(file));
-                        clip.start();
+                        clip.loop(Clip.LOOP_CONTINUOUSLY);
                         Thread.sleep(clip.getMicrosecondLength());
-                    } catch (LineUnavailableException e) {
-                        e.printStackTrace();
-                    } catch (UnsupportedAudioFileException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+
+                } catch (LineUnavailableException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedAudioFileException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-                    .start();
 
-        }
 
+        };
+
+        derp.setDaemon(true);
+        derp.start();
+
+    }
 }
+
